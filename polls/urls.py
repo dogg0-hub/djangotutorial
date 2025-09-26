40415581e622a65b3c812f261ba29591825a67c3
dogg0-hub <konnichidoggo@gmail.com>
2025-09-26 18:41:02 +0900

    中完成

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 #path("URLのパターン", 実行するビュー関数, name="名前")
+app_name ="polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,73 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question, Choice
+from django.template import loader
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.db.models import F
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello,world you'are at the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    #template = loader.get_template("polls/index.html")
+    context = {"latest_question_list":
+            latest_question_list}
+    return render(request, "polls/index.html", context)
+#render() 関数は、第1引数として request オブジェクトを、
+# 第2引数としてテンプレート名を、
+# 第3引数（任意）として辞書を受け取ります
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    #get_object_or_404() 関数は、
+    # Django モデルを第一引数に、
+    # 任意の数のキーワード引数を取り、
+    # モデルのマネージャの get() 関数に渡します。
+    # オブジェクトが存在しない場合は 
+    # Http404 を発生させます。
+    question = get_object_or_404(Question,
+                                pk=question_id)
+    return render(request, "polls/detail.html",
+                {"question":question})
+
+#def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exits")
+    return render(request, "polls/detail.html",
+                {"question":question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question,
+                                pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message" : "You didn't select a choice."
+            }
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # POST データが成功した後は
+        # 常に HttpResponseRedirect を返す必要がある
+        return HttpResponseRedirect(reverse("polls:results",
+                                            args=(question.id,)))
 
+#def vote(request, question_id):
+#    return HttpResponse("You're voting on question %s." % question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question,
+                                pk=question_id)
+    return render(request,"polls/results.html",
+                {"question":question})
